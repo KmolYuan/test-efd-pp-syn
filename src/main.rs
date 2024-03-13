a317835b89779ff0a7cfcc7c648d9973d16debe4
KmolYuan <pyslvs@gmail.com>
2024-03-13 13:57:41 +0800

    Update progress

--- src/main.rs
+++ src/main.rs
@@ -30,15 +30,13 @@ where
 }
 
 fn main() {
-    // ../four-bar-rs/test-fb/slice.open.csv
-    // test.csv
-    // test2.csv
-    let w = std::fs::File::open("test.csv").unwrap();
+    // [Ref] ../four-bar-rs/test-fb/slice.open.csv
+    let w = std::fs::File::open("test3.csv").unwrap();
     let target = csv::from_reader(w).unwrap();
-    let curve1 = syn_test(1, FbPPSyn::from_curve(&target, Mode::Open)).curve(90);
-    let curve2 = syn_test(2, FbSyn::from_curve(&target, Mode::Open)).curve(90);
+    let curve1 = syn_test(1, FbPPSyn::from_curve(&target, Mode::Closed)).curve(90);
+    let curve2 = syn_test(2, FbSyn::from_curve(&target, Mode::Closed)).curve(90);
     plot::fb::Figure::new(None)
-        .legend(LegendPos::MR)
+        .legend(LegendPos::UL)
         .add_line("Target", &target, Style::Circle, RED)
         .add_line("P-P. Optimized", &curve1, Style::Line, BLUE)
         .add_line("Optimized", &curve2, Style::Line, full_palette::ORANGE_900)
